aa72adf22792ea3e5ea6849b47cb7d18dd1c7c00
Adam R <aroulhac92@gmail.com>
2019-07-18 01:27:11 -0400

    07182019 changes include incomplete transition to flexbox for entire app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BudgetComponent } from './components/budget/budget.component'
+import { CalculatorComponent } from './components/calculator/calculator.component'
+import { HomeComponent } from './components/home/home.component'
+import { PersonalPlannerComponent } from './components/personal-planner/personal-planner.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "budget", component: BudgetComponent},
+  {path: "calc", component: CalculatorComponent},
+  {path: "pp", component: PersonalPlannerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalPlannerComponent } from './components/personal-planner/personal-planner.component';
+import { BudgetComponent } from './components/budget/budget.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { CryptocurrencyComponent } from './components/home/cryptocurrency/cryptocurrency.component';
+import { StockWatchComponent } from './components/home/stock-watch/stock-watch.component';
+import { AdviceComponent } from './components/home/advice/advice.component';
+import { TechStocksComponent } from './components/home/tech-stocks/tech-stocks.component';
+import { MyStockWatchComponent } from './components/home/my-stock-watch/my-stock-watch.component';
+import { SearchStocksComponent } from './components/home/search-stocks/search-stocks.component';
+import { NotesComponent } from './components/home/notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalPlannerComponent,
+    BudgetComponent,
+    CalculatorComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    CryptocurrencyComponent,
+    StockWatchComponent,
+    AdviceComponent,
+    TechStocksComponent,
+    MyStockWatchComponent,
+    SearchStocksComponent,
+    NotesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
